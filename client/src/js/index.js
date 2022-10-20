517092a326f41905a57a0cd51da6553527e4c855
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-10-19 20:03:31 -0400

    Create IndexedDB through npm idb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -5,12 +5,15 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/index.css";
 
+import { initdb } from './database';
+
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
